47b7bf72675bd9ac39347237bc0fea36e5b977f0
Mevlut Arslan <mevlutcaferarslan@outlook.com>
2021-04-14 16:44:27 +0200

    Implement basic spawn system

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    public Wave[] waves;
+    public Enemy enemy;
+
+    private int enemiesRemainingToSpawn;
+    private float nextSpawnTime;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+
+    private void Start()
+    {
+        NextWave();
+    }
+
+    private void Update()
+    {
+        if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime)
+        {
+            enemiesRemainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+            
+            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+        }
+    }
+
+    void NextWave()
+    {
+        currentWaveNumber ++;
+
+        currentWave = waves[currentWaveNumber - 1];
+
+        enemiesRemainingToSpawn = currentWave.enemyCount;
+
+    }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public int enemyCount;
+        public float timeBetweenSpawns;
+    }
+    
+    
+}
